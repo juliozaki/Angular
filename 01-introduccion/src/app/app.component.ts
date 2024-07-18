417c7fd5a9e56e1059e73aee1160aba88b860806
juliozaki <cliff_metallica_@hotmail.com>
2024-07-17 19:16:46 -0600

    estructura del proyecto

--- 01-introduccion/src/app/app.component.ts
+++ 01-introduccion/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppCounter } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppCounter],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
